f00a528a3c1600db03d7a0b98e4dcf6994235b41
UltraCube73 <57032834+UltraCube73@users.noreply.github.com>
2021-04-10 01:47:00 +0400

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-situation = 'start'
-
-
-while True:
-    main.choice = ''
-    system(main.clcmd)
-    cur_situation = getattr(main, situation)
-    cur_situation()
-    situation = main.next_situation
